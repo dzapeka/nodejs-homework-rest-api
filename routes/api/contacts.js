61c042fd80b25b67477c657ac2b11354a1e4cff5
dzapeka <douson@gmail.com>
2023-12-30 02:11:51 +0100

    implement api functions

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,64 @@
-const express = require('express')
-
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const contactsSchema = require(path.join(__dirname, '../../schemas/contacts'));
+const Contacts = require(path.join(__dirname, '../../contacts.js'));
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  res.json(await Contacts.getAllContacts());
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const { contactId } = req.params;
+  const contact = await Contacts.getContactById(contactId);
+  if (!contact) return res.status(404).json({ message: 'Not found' });
+  res.json(contact);
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const response = contactsSchema.validate(req.body, { abortEarly: false });
+
+  if (typeof response.error !== 'undefined') {
+    return res
+      .status(400)
+      .send(response.error.details.map(err => err.message).join(', '));
+  }
+
+  const { name, email, phone } = response.value;
+  const newContact = await Contacts.addContact(name, email, phone);
+
+  res.status(201).json(newContact);
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const { contactId } = req.params;
+  const deletedContact = await Contacts.removeContact(contactId);
+  if (!deletedContact) return res.status(404).json({ message: 'Not found' });
+  res.json({ message: 'contact deleted' });
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const { contactId } = req.params;
 
-module.exports = router
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).json({ message: 'missing fields' });
+  }
+
+  const response = contactsSchema.validate(req.body, { abortEarly: false });
+  if (typeof response.error !== 'undefined') {
+    return res
+      .status(400)
+      .send(response.error.details.map(err => err.message).join(', '));
+  }
+
+  const updatedContact = await Contacts.updateContact(
+    contactId,
+    response.value
+  );
+
+  if (!updatedContact) return res.status(404).json({ message: 'Not found' });
+
+  res.json(updatedContact);
+});
+
+module.exports = router;
