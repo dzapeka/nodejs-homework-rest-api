201eba96aabeafa6d11cb913da732b74aa6bfed2
Dmytro Zapeka <douson@gmail.com>
2024-01-27 19:16:31 +0100

    Implement user authentication feature (#3)

--- models/contact.js
+++ models/contact.js
@@ -1,21 +1,29 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'user',
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false, timestamps: false }
+);
 
 module.exports = mongoose.model('Contact', contactSchema);
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const contactsRouter = require('./contacts');
 
-router.use('/contacts', contactsRouter);
+const authMiddleware = require('../middlewares/auth');
+router.use('/contacts', authMiddleware, contactsRouter);
 
 module.exports = router;
